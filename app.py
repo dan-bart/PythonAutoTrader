0758dba497661c17b5da8d50d0ebc6c7447d5d52
dan-bart <dan.bartusek@seznam.cz>
2021-05-10 11:18:57 +0200

    working trader, flask draft

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
-import os
 import trader
+import yfinance
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, url_for
 from flask_bootstrap import Bootstrap
-from bokeh.embed import components
-import bokeh
+from flask_table import Table, Col
+
+import bokeh.sampledata
+import pandas as pd
+from bokeh.layouts import gridplot
+from bokeh.plotting import figure, output_file, show
+from bokeh.sampledata.stocks import AAPL, GOOG, IBM, MSFT
+from math import pi
+import numpy as np
+
+
 
 sql_inst = trader.SQL()
-
 app = Flask(__name__)
 
+#data from DB
 portfolio = sql_inst.fetch_all("portfolio")   
 trade_ideas = sql_inst.fetch_all("discord_trade_ideas")
 records = sql_inst.fetch_all("records")
+
+
 
 @app.route("/", methods=["GET"])
 @app.route("/home", methods=["GET"])
@@ -26,7 +37,32 @@ def stock_data(stock):
 
 @app.route("/portfolio")
 def portfolio_fun():
-    return render_template("df.html.j2", length=portfolio.shape[1], dataframe=portfolio.to_html(),page = '/portfolio')
+    df = pd.DataFrame(MSFT)[:50]
+    df["date"] = pd.to_datetime(df["date"])
+
+    inc = df.close > df.open
+    dec = df.open > df.close
+    w = 12*60*60*1000 # half day in ms
+
+    TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
+
+    p = figure(x_axis_type="datetime", tools=TOOLS, plot_width=1000, title = "MSFT Candlestick")
+    p.xaxis.major_label_orientation = pi/4
+    p.grid.grid_line_alpha=0.3
+
+    p.segment(df.date, df.high, df.date, df.low, color="black")
+    p.vbar(df.date[inc], w, df.open[inc], df.close[inc], fill_color="#D5E1DD", line_color="black")
+    p.vbar(df.date[dec], w, df.open[dec], df.close[dec], fill_color="#F2583E", line_color="black")
+    show(p)
+
+
+    kwargs = {'script': script, 'div': div}
+    kwargs['title'] = 'bokeh-with-flask' 
+
+    df_portfolio = portfolio.style.highlight_max(axis=0, subset=['avg_buy']).render()
+    
+
+    return render_template("df.html.j2", length=portfolio.shape[1], dataframe=df_portfolio,page = '/portfolio', **kwargs)
 
 @app.route("/ideas")
 def ideas_fun():
@@ -36,15 +72,17 @@ def ideas_fun():
 def records_fun():
     return render_template("df.html.j2", length=records.shape[1], dataframe=records.to_html())
 
-
-
-
 @app.route("/dfcustom")
 def dfcustom():
     data = portfolio.to_dict(orient="records")
     headers = portfolio.columns
     print(headers)
     return render_template("dfcustom.html.j2", data=data, headers=headers)
+
+
+
+def datetime(x):
+    return np.array(x, dtype=np.datetime64)
 
 
 # @app.route("/bokehplot")
@@ -60,3 +98,38 @@ def dfcustom():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+def plot_stock(stock):
+    p1 = figure(x_axis_type="datetime", title="Stock Closing Prices")
+    p1.grid.grid_line_alpha=0.3
+    p1.xaxis.axis_label = 'Date'
+    p1.yaxis.axis_label = 'Price'
+    p1.line(x=datetime(AAPL['date']), y=AAPL['adj_close'], color='#A6CEE3', legend_label='AAPL')
+    p1.legend.location = "top_left"
+
+    aapl = np.array(AAPL['adj_close'])
+    print(AAPL['adj_close'])
+    print(datetime(AAPL['date']))
+    aapl_dates = np.array(AAPL['date'], dtype=np.datetime64)
+
+    window_size = 30
+    window = np.ones(window_size)/float(window_size)
+    aapl_avg = np.convolve(aapl, window, 'same')
+
+    p2 = figure(x_axis_type="datetime", title="AAPL One-Month Average")
+    p2.grid.grid_line_alpha = 0
+    p2.xaxis.axis_label = 'Date'
+    p2.yaxis.axis_label = 'Price'
+    p2.ygrid.band_fill_color = "olive"
+    p2.ygrid.band_fill_alpha = 0.1
+
+    p2.circle(x=aapl_dates, y=aapl, size=4, legend_label='close',
+            color='darkgrey', alpha=0.2)
+
+    p2.line(x=aapl_dates, y=aapl_avg, legend_label='avg', color='navy')
+    p2.legend.location = "top_left"
+
+    output_file("stocks.html", title="stocks.py example")
+
+    show(gridplot(children = [[p1,p2]], plot_width=400, plot_height=400))  # open a browser
--- trader.py
+++ trader.py
@@ -6,8 +6,8 @@ from sqlalchemy import create_engine
 import sqlalchemy
 import maya
 import yfinance as yf
-from datetime import datetime
-from datetime import time
+import datetime as dt
+import time
 import psycopg2
 import warnings
 import schedule
@@ -19,7 +19,7 @@ def get_messages(channel_id):
     Gets a json of trade ideas from stock discord channel
     
     '''
-    headers = {'authorization': 'MTM1NzI0Mjg3OTA1NDk3MDg4.YFyz4g.fsP2iLpU6GcFjZ34PjAkOd8wqas'}
+    headers = {'authorization': 'MTM1NzI0Mjg3OTA1NDk3MDg4.YJJszA.Gzd4o-aCP61oPufovkzA4pPzOAk'}
     r = requests.get(f"https://discord.com/api/v8/channels/{channel_id}/messages", headers = headers)
     js = json.loads(r.text)
 
@@ -54,7 +54,7 @@ def message_parser(stock):
             for j in stock[i:]:
                 if re.match(r'[$]',j):
                     stop = [x.group(1) for x in regex.finditer(j)]
-                    dfStop = pd.DataFrame([[ticker,stop[0],'support']],columns = ['ticker','price','type'])                
+                    dfStop = pd.DataFrame([[ticker,stop[0],'stop']],columns = ['ticker','price','type'])                
                     break
     try:         
         df_full = pd.concat([dfSup,dfpt,dfStop],ignore_index=True)
@@ -80,6 +80,7 @@ class SQL:
                                 ticker VARCHAR(10) PRIMARY KEY, 
                                 owned INT,
                                 avg_buy FLOAT,
+                                last_hour FLOAT,
                                 hourly_state FLOAT) """
 
         self.sql_create_records= """ CREATE TABLE IF NOT EXISTS records (
@@ -87,7 +88,9 @@ class SQL:
                             ammount INT,
                             price FLOAT,
                             date timestamp) """
+        self.create_tables()
 
+    def create_tables(self):
         self.connection = psycopg2.connect("dbname='stock_project' user='postgres' host='localhost' password='1234'")
         self.connection.autocommit = True 
         self.engine = create_engine('postgresql://postgres:1234@localhost:5432/stock_project')
@@ -96,6 +99,15 @@ class SQL:
         for sql_statement in [self.sql_create_trade_ideas, self.sql_create_portfolio, self.sql_create_records]:
             print(sql_statement)
             self.cursor.execute(sql_statement)
+
+
+    def reset_tables(self):
+        self.cursor.execute(f"delete from discord_trade_ideas")
+        self.cursor.execute(f"delete from portfolio")
+        self.cursor.execute(f"delete from records")
+        self.create_tables()
+
+
 
     def fetch_all(self,table):
         '''
@@ -106,8 +118,8 @@ class SQL:
         return df
     def activate_level(self,ticker,price_type, price,status):
         self.cursor.execute(f"UPDATE discord_trade_ideas SET activated = {status} WHERE (ticker = '{ticker}' and type = '{price_type}' and price = '{price}')")
-    def update_portfolio(self,ticker,owned,avg_buy,state):
-        self.cursor.execute(f"UPDATE portfolio SET owned = {owned},hourly_state = {state}, avg_buy = {avg_buy} WHERE ticker = '{ticker}'")
+    def update_portfolio(self,ticker,owned,avg_buy,last_hour,state):
+        self.cursor.execute(f"UPDATE portfolio SET owned = {owned},hourly_state = {state}, avg_buy = {avg_buy}, last_hour = {last_hour} WHERE ticker = '{ticker}'")
     def write_trade_idea(self,row):
         self.cursor.execute("INSERT INTO discord_trade_ideas (ticker, price, type, edited, activated) VALUES (%s, %s, %s, %s, %s)", (row['ticker'], row['price'], row['type'], row['edited'], row['activated']))
     def write_portfolio(self,row):
@@ -117,15 +129,41 @@ class SQL:
 
 
 class Trader():
-    def __init__(self, trade_ideas, portfolio,sql_inst):
+    def __init__(self, trade_ideas,sql_inst):
         self.trade_ideas = trade_ideas
-        self.portfolio = portfolio
         self.sql_inst = sql_inst
+        self.portfolio = sql_inst.fetch_all('portfolio')
+        self.cash_value = self.portfolio.loc[self.portfolio['ticker'] == 'cash','hourly_state'].iloc[0]
+
+
+    #close trade if we are below the stop out on the daily close
+    def check_daily_close(self,ticker):
+        #necessary data
+        data = yf.download(tickers = ticker, period = '1d',interval = '1h')
+        stop = self.trade_ideas.loc[(self.trade_ideas['type'] == 'stop') & (self.trade_ideas['ticker'] == ticker)].sort_values(by = 'price').reset_index()
+        supports = self.trade_ideas.loc[(self.trade_ideas['type'] != 'pt') & (self.trade_ideas['ticker'] == ticker)].sort_values(by = 'price').reset_index()
+        pts = self.trade_ideas.loc[(self.trade_ideas['type'] == 'pt') & (self.trade_ideas['ticker'] == ticker)].sort_values(by = 'price',ascending = False).reset_index() # FROM HIGHEST
+        latestOpen = data.iloc[len(data)-1]['Low']    
+        owned = self.portfolio.loc[self.portfolio['ticker'] == ticker,'owned'].iloc[0]
+        avg_buy = self.portfolio.loc[self.portfolio['ticker'] == ticker,'avg_buy'].iloc[0] 
+
+        if(latestOpen<stop.iloc[0]['price'] and owned>0):
+            state = latestOpen*owned-avg_buy*owned
+            self.sql_inst.update_portfolio(ticker,0,avg_buy,latestOpen,state)
+            self.cash_value = self.cash_value+latestOpen*owned
+            self.sql_inst.update_portfolio('cash',0,0,latestOpen,self.cash_value)
+            #reset all trade idea levels for this ticker
+            for row in pts:
+                self.sql_inst.activate_level(row['ticker'],row['type'],row['price'],False)
+            for row in supports:
+                self.sql_inst.activate_level(row['ticker'],row['type'],row['price'],False)                
+            return [ticker,-owned,latestOpen,dt.datetime.now()]
+             
 
     def check_trade(self,ticker):
         #necessary data
         data = yf.download(tickers = ticker, period = '1d',interval = '1h')
-        supports = self.trade_ideas.loc[(self.trade_ideas['type'] != 'pt') & (self.trade_ideas['ticker'] == ticker)].sort_values(by = 'price').reset_index()
+        supports = self.trade_ideas.loc[(self.trade_ideas['type'] == 'support') & (self.trade_ideas['ticker'] == ticker)].sort_values(by = 'price').reset_index()
         pts = self.trade_ideas.loc[(self.trade_ideas['type'] == 'pt') & (self.trade_ideas['ticker'] == ticker)].sort_values(by = 'price',ascending = False).reset_index() # FROM HIGHEST
         last_hour = data.iloc[len(data)-2]
         latestOpen = data.iloc[len(data)-1]['Open']    
@@ -137,20 +175,19 @@ class Trader():
         print(pts)
         print(round(last_hour,2))
         print(latestOpen)
-
-        cash_value = self.portfolio.loc[self.portfolio['ticker'] == 'cash','hourly_state'].iloc[0]
         #check if the lowest support (stop-out) has been broken
         #sell this stock
         if(last_hour['Close']<supports.iloc[0]['price'] and owned>0):
-            state = avg_buy*owned-latestOpen*owned
-            self.sql_inst.update_portfolio(ticker,0,avg_buy,state)
-            self.sql_inst.update_portfolio('cash',0,0,cash_value+latestOpen*owned)
+            state = latestOpen*owned-avg_buy*owned
+            self.sql_inst.update_portfolio(ticker,0,avg_buy,latestOpen,state)
+            self.cash_value = self.cash_value+latestOpen*owned
+            self.sql_inst.update_portfolio('cash',0,0,latestOpen,self.cash_value)
             #reset all trade idea levels for this ticker
             for row in pts:
                 self.sql_inst.activate_level(row['ticker'],row['type'],row['price'],False)
             for row in supports:
                 self.sql_inst.activate_level(row['ticker'],row['type'],row['price'],False)                
-            return [ticker,-owned,latestOpen,datetime.now()]
+            return [ticker,-owned,latestOpen,dt.datetime.now()]
 
         #bounce of some level (support) - buy
         if (last_hour['Open']>last_hour['Low'] and last_hour['Close']>last_hour['Low']):
@@ -167,9 +204,7 @@ class Trader():
 
             # check if the level which the price action bounced of was actually the defined support
             #current price is very close to our support but hourly close is above the support -> buy
-            if (len(curr_lvl) == 0):
-                return None
-            if ((last_hour['Low']+0.1>=curr_lvl['price']) and (last_hour['Low'] - curr_lvl['price']<0.5)  and last_hour['Close']>=curr_lvl['price']): 
+            if ((len(curr_lvl) != 0) and (last_hour['Low']+0.1>=curr_lvl['price']) and (last_hour['Low'] - curr_lvl['price']<0.5)  and last_hour['Close']>=curr_lvl['price']): 
                 if(supports.loc[supports['price'] == curr_lvl['price'],'activated'].iloc[0] == False): 
                     supports.loc[supports['price'] == curr_lvl['price'],'activated'] = True
                     self.sql_inst.activate_level(ticker,curr_lvl['type'],curr_lvl['price'],True)
@@ -178,10 +213,11 @@ class Trader():
                     purchase = (len(supports)-i)*2
                     avg_buy = (avg_buy*owned + purchase*latestOpen)/(owned+purchase)
                     owned += purchase
-                    state = avg_buy*owned-latestOpen*owned                
-                    self.sql_inst.update_portfolio(ticker,owned,avg_buy,state)
-                    self.sql_inst.update_portfolio('cash',0,0,cash_value-latestOpen*purchase)
-                    return [ticker,purchase,latestOpen,datetime.now()]
+                    state = latestOpen*owned-avg_buy*owned              
+                    self.sql_inst.update_portfolio(ticker,owned,avg_buy,latestOpen,state)
+                    self.cash_value = self.cash_value-latestOpen*purchase
+                    self.sql_inst.update_portfolio('cash',0,0,latestOpen,self.cash_value)
+                    return [ticker,purchase,latestOpen,dt.datetime.now()]
 
         #price climbed up -> might have hit a price target -> sell some    
         elif(last_hour['Open']<last_hour['Close'] and owned >0):
@@ -191,9 +227,10 @@ class Trader():
                     if((pts.iloc[i]['price'] - last_hour['Close']< 0.5) or last_hour['Close']>pts.iloc[i]['price']): #very close to price target or above
                         
                         if (i == 0): # highest price target -> sell all
-                            state = avg_buy,avg_buy*owned-latestOpen*owned
-                            self.sql_inst.update_portfolio(ticker,0,avg_buy,state)                               
-                            self.sql_inst.update_portfolio('cash',0,0,cash_value-latestOpen*owned)
+                            state = latestOpen*owned-avg_buy*owned
+                            self.sql_inst.update_portfolio(ticker,0,avg_buy,latestOpen,state)   
+                            self.cash_value = self.cash_value-latestOpen*owned                         
+                            self.sql_inst.update_portfolio('cash',0,0,latestOpen,self.cash_value)
                             
                             #reset all trade idea levels for this ticker
                             for row in pts:
@@ -201,7 +238,7 @@ class Trader():
                             for row in supports:
                                 self.sql_inst.activate_level(row['ticker'],row['type'],row['price'],False)  
 
-                            return [ticker,-owned,latestOpen,datetime.now()]
+                            return [ticker,-owned,latestOpen,dt.datetime.now()]
 
                         else: #sell proportional
                             pts.loc[pts['price'] == row['price'],'activated'] = True
@@ -210,34 +247,61 @@ class Trader():
                                 sell = owned*i/len(pts.index)
                             else:
                                 sell = owned
-                            state = avg_buy*owned-latestOpen*owned
-                            self.sql_inst.update_portfolio('cash',0,0,cash_value+latestOpen*sell)                            
-                            self.sql_inst.update_portfolio(ticker,owned-sell,avg_buy,state) 
-                            return [ticker,-sell,latestOpen,datetime.now()]         
+                            state = latestOpen*owned-avg_buy*owned
+                            self.cash_value = self.cash_value+latestOpen*sell
+                            self.sql_inst.update_portfolio('cash',0,0,latestOpen,self.cash_value)                            
+                            self.sql_inst.update_portfolio(ticker,owned-sell,avg_buy,latestOpen,state) 
+                            return [ticker,-sell,latestOpen,dt.datetime.now()]
+
+        #no new trade, just update hourly state
+        state = latestOpen*owned-avg_buy*owned
+        self.sql_inst.update_portfolio(ticker,owned,avg_buy,latestOpen,state)         
         return None
 
+def isNowInTimePeriod(startTime, endTime, nowTime): 
+    if startTime < endTime: 
+        return nowTime >= startTime and nowTime <= endTime 
+    else: 
+        #Over midnight: 
+        return nowTime >= startTime or nowTime <= endTime 
+
+def check_close():
+    sql_inst = SQL()
+    all_ideas = sql_inst.fetch_all('discord_trade_ideas')
+    portfolio = sql_inst.fetch_all('portfolio')
+    tr = Trader(all_ideas,sql_inst)
+    for i in all_ideas['ticker'].unique():
+        curr_trade = tr.check_daily_close(i)
+        if curr_trade is not None:
+            #update records with new trade              
+            sql_inst.write_record(curr_trade)
+   
 def hourly_update():
     sql_inst = SQL()
-    all_trades = sql_inst.fetch_all('discord_trade_ideas')
+    all_ideas = sql_inst.fetch_all('discord_trade_ideas')
     #check if cash value has already been defined
     if (len(sql_inst.fetch_all('portfolio')) == 0):
-        sql_inst.write_portfolio(['cash',0,0,10000])
+        sql_inst.write_portfolio(['cash',0,0,100000])
     portfolio = sql_inst.fetch_all('portfolio')
-    tr = Trader(all_trades,portfolio,sql_inst)
+    tr = Trader(all_ideas,sql_inst)
 
-    for i in all_trades['ticker'].unique():
+    for i in all_ideas['ticker'].unique():
         #adding new ticker
-        if(i not in tr.portfolio['ticker']):
+        if(i not in portfolio['ticker'].values):
             sql_inst.write_portfolio([i,0,0,0])
-            tr.portfolio = tr.portfolio.append({'ticker': i, 'owned': 0, 'avg_buy': 0, 'hourly_state': 0}, ignore_index = True)
-            print(tr.portfolio )
+            new_row = {'ticker':i,'owned':0,'avg_buy':0,'last_hour  ':0,'hourly_state': 0}
+            tr.portfolio = tr.portfolio.append(new_row, ignore_index = True)
+            print(tr.portfolio)
         curr_trade = tr.check_trade(i)
         if curr_trade is not None:
             #update records with new trade              
             sql_inst.write_record(curr_trade)
+
+
 def main():
     if False:
         sql_inst = SQL()
+        sql_inst.reset_tables()
         stock_ideas =  '814856374663249971'
         all_ideas = []
         mess = get_messages(stock_ideas)
@@ -258,15 +322,28 @@ def main():
         for index, row in df_full.iterrows():
             sql_inst.write_trade_idea(row)
 
-    hourly_update()
-    # warnings.simplefilter(action='ignore', category=FutureWarning)
-    # if (datetime.now().time() >= time(15,31,00)):
-    #     hourly_update()
+    
+    #hourly_update()
+    warnings.simplefilter(action='ignore', category=FutureWarning)
 
-    # schedule.every().hour.do(hourly_update)
-    # while (datetime.now().time() >= time(15,31,00) and datetime.now().time() <= time(21,35,00)):
-    #     schedule.run_pending()
+    #start hourly trading
+    while True:
+        if (dt.datetime.now().time() >=dt.time(16,31)):
+            hourly_update()
+            break
+        time.sleep(10)
+    schedule.every().hour.do(hourly_update)
+    #stop at 21:55
+    while(dt.datetime.now().time() <= dt.time(21,55)):
+        schedule.run_pending()
+        time.sleep(10)
 
+    #check closes below stop-outs
+    while True:
+        if (dt.datetime.now().time() >=dt.time(21,8)):
+            check_close()
+            break
+        time.sleep(10)
 
 
 if __name__ == "__main__":
